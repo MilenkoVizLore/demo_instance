8cba3a653f67dfbb579a0d534497d4b0e6129886
ALojdl <andrej.lojdl@gmail.com>
2015-03-02 17:38:03 +0100

    Solved a bug.

--- conrec/toolbox.py
+++ conrec/toolbox.py
@@ -218,7 +218,10 @@ def get_recommendation(time_stamp, coordinates, user_id, ignore):
     if 'error' in act_rest_answer:
         activity = 3  # If activity recognition provider encountered some error.
     else:
-        activity = get_curr_activity(act_rest_answer['svm_vector'])
+        req_act = dict()
+        for k, v in act_rest_answer['svm_vector'].iteritems():
+            req_act[k] = float(v)
+        activity = get_curr_activity(req_act)
 
     ''' Get all POIs in radius of 300 meters from user. '''
     points_of_interest = get_poi(coordinates['lat'], coordinates['lon'], 300)
